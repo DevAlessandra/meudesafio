b696b25cb257d66114d2bc81f3c81b3fc1597f59
Alessandra Santana <alecorretorainova@gmail.com>
2026-03-16 00:09:59 -0300

    controle financeiro

---
+++ src/Dashboard.js
@@ -0,0 +1,10 @@
+function Dashboard({ usuario }) {
+  return (
+    <div>
+      <h2>Olá, {usuario}!</h2>
+      {/* aqui entra todo o seu código atual de resumo financeiro e transações */}
+    </div>
+  );
+}
+
+export default Dashboard;
