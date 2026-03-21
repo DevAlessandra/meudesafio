3f028bb92390d88c48633d27fd89f3e34df55e27
Alessandra Santana <alecorretorainova@gmail.com>
2026-03-21 20:42:18 -0300

    criado initDb

---
+++ initDb.js
@@ -0,0 +1,31 @@
+import pool from "./db.js";
+
+async function initDb() {
+  try {
+    await pool.query(`
+      CREATE TABLE IF NOT EXISTS usuarios (
+        id SERIAL PRIMARY KEY,
+        nome VARCHAR(100) NOT NULL,
+        email VARCHAR(100) UNIQUE NOT NULL,
+        senha VARCHAR(200) NOT NULL
+      );
+    `);
+
+    await pool.query(`
+      CREATE TABLE IF NOT EXISTS transacoes (
+        id SERIAL PRIMARY KEY,
+        descricao VARCHAR(255) NOT NULL,
+        data DATE NOT NULL,
+        valor NUMERIC(10,2) NOT NULL,
+        tipo VARCHAR(10) CHECK (tipo IN ('entrada','saida')) NOT NULL,
+        usuario_id INTEGER REFERENCES usuarios(id) ON DELETE CASCADE
+      );
+    `);
+
+    console.log("✅ Tabelas verificadas/criadas com sucesso");
+  } catch (err) {
+    console.error("Erro ao inicializar banco:", err);
+  }
+}
+
+initDb();
